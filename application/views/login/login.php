c137d93e2b34e0d55d47d33ddddd8e8ffc3f96a0
wegstar <wegstar@gmail.com>
2012-01-28 18:32:16 -0800

    Styled up the login.php and register.php views.
    Stylized the login.php and register.php views.

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,14 +1,55 @@
 <html>
+
 <head>
 <title>Login</title>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
 </head>
+
 <body>
-<form action="" method="POST">
+
+<div id="header"></div> <div id="logo"></div>
+<div id="menu"></div>
+<div id="content">
+
+<center>
+<div class="head">Login</div>
+</center>
+
+<div class="box">
+<form action="" method="POST" class="body" >
 <?= $error ?><br>
-Username: <input type="text" name="username"><br>
-Password: <input type="password" name="password"><br>
+
+<table>
+
+<tr>
+<td>
+<center>
+<div class="label">Username:&nbsp;&nbsp;
+<input type="text" name="username"></div></td>
+</center>
+</tr>
+
+<tr>
+<td>
+<center>
+<div class="label">Password:&nbsp;&nbsp;
+<input type="password" name="password"></div>
+</center>
+</td>
+</tr>
+
+</table>
+
 <?php if($redirect) { echo '<input type="hidden" name="redirect" value="'.$redirect.'">'; }?><br>
+
+<center>
 <input type="submit" name="submit" value="Login">
+</center>
+
+</div>
+
 </form>
+</div>
+<div id="footer"> </div>
 </body>
 </html>
--- application/views/register/register.php
+++ application/views/register/register.php
@@ -1,17 +1,86 @@
 <html>
 <head>
-<title>Login</title>
+<title>Register</title>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
 </head>
+
 <body>
+
 <?= validation_errors(); ?>
 <?= form_open('register'); ?>
-Username: <input type="text" name="username" value="<?= set_value('username') ?>"><br>
-Email: <input type="text" name="email" value="<?= set_value('email') ?>"><br>
-Password: <input type="password" name="password"><br>
-Verify Password: <input type="password" name="passconf"><br>
-<input type="checkbox" name="rules" value="rules" <?= set_checkbox("rules", "rules"); ?>> I will read the rules.<br>
-<input type="checkbox" name="age" value="age" <?= set_checkbox("age", "age"); ?>> I am 13 years of age or older.<br>
+
+<div id="header"> </div><div id="logo"></div>
+<div id="menu"></div>
+
+<div id="content">
+<center>
+<div class="head">Registration</div>
+</center>
+
+<div class="box">
+
+<form action="" method="POST" class="body" >
+<?= $error ?><br>
+
+<table>
+
+<tr>
+<td>
+<div class="label">Desired Username:;</div>
+</td>
+<td><input type="text" name="username" value="<?= set_value('username') ?>"></td>
+</tr>
+
+<tr>
+<td>
+<div class="label">Email:</div>
+</td>
+<td><input type="text" name="email" value="<?= set_value('email') ?>"></td>
+</tr>
+
+
+<tr>
+<td>
+<div class="label">Password:</div>
+</td>
+<td><input type="password" name="password"></td>
+</tr>
+
+<tr>
+<td>
+<div class="label">Verify Password:</div>
+</td>
+<td><input type="password" name="passconf"></td>
+</tr>
+
+
+<tr>
+<td>
+<div class="label">I will read the rules</div>
+</td>
+<td><input type="checkbox" name="rules" value="rules" <?= set_checkbox("rules", "rules"); ?> ></td>
+</tr>
+
+<tr>
+<td>
+<div class="label">I am 13 years of age or older.</div>
+</td>
+<td><input type="checkbox" name="age" value="age" <?= set_checkbox("age", "age"); ?> ></td>
+</tr>
+
+
+</table>
+
+
+<center>
 <br><input type="submit" name="submit" value="Register">
+</center>
+
+</div>
+
 </form>
+
+</div>
+<div id="footer"> </div>
 </body>
 </html>
