c4676f65f9969b71ba7ea03155e21ffda188c40c
Phos <phosphos0@gmail.com>
2012-03-04 23:15:11 -0800

    Forgot a </a>

--- application/views/kb/view.php
+++ application/views/kb/view.php
@@ -1,6 +1,6 @@
 <h3><?= htmlspecialchars($article['name']) ?></h3>
 
 <div class="kb_article_body">
-  <span class="right"><a href="/kb/edit/<?= $article['id'] ?>">Edit</span>
+  <span class="right"><a href="/kb/edit/<?= $article['id'] ?>">Edit</a></span>
   <?= $article['html_src'] ?>
 </div>
