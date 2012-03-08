894620f98146d5b8cc30173e884355048c86e5d3
aegamesi <admin.aegamesi@gmail.com>
2012-03-08 02:10:45 -0500

    beautified kb article view

--- application/views/kb/view.php
+++ application/views/kb/view.php
@@ -1,8 +1,10 @@
-<h3><?= htmlspecialchars($article['name']) ?></h3>
-
-<div class="kb_article_body">
-  <?php if ($can_edit) { ?>
-    <span class="right"><a href="/kb/edit/<?= $article['id'] ?>">Edit</a> | <a href="/kb/delete/<?= $article['id'] ?>">Delete</a></span>
-  <?php } ?>
-  <?= $article['html_src'] ?>
+<div class="box">
+	<div class="head"><?= htmlspecialchars($article['name']) ?>
+		<?php if ($can_edit) { ?>
+    		<span class="right"><a href="/kb/edit/<?= $article['id'] ?>">Edit</a> | <a href="/kb/delete/<?= $article['id'] ?>">Delete</a></span>
+  		<?php } ?>
+  	</div>
+	<div class="pad">
+		<?= $article['html_src'] ?>
+	</div>
 </div>
